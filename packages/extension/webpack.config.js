a86401ddc5f5455bab3e47717f433ce1e4aba0ee
Gabriel Huttemann <gabehutt@umich.edu>
2025-11-11 16:28:56 -0500

    Manifest file fixes, Package management fixes, adds logo images

--- packages/extension/webpack.config.js
+++ packages/extension/webpack.config.js
@@ -1,3 +1,4 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -22,5 +23,25 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
-  }
+  },
+  plugins: [
+    // ...any other plugins you might have
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          // Assumes your manifest is at 'packages/extension/src/manifest.json'
+          // Change 'src/manifest.json' if it's located somewhere else
+          from: 'manifest.json', 
+          to: 'manifest.json' // This will copy it to the root of dist/
+        },
+        {// --- ADD THIS BLOCK ---
+          // This copies your images folder
+          from: 'src/images',
+          to: 'images'
+          // --- END OF NEW BLOCK ---
+        }
+      ]
+    })
+  ]
 };
